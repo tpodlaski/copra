06b4fa760f83a5920deb517f6f4e3c9a7c34a436
Tony Podlaski <tony@podlaski.com>
2018-07-11 22:04:09 +0000

    Updated copra.websocket docstrings.

--- copra/websocket.py
+++ copra/websocket.py
@@ -38,7 +38,8 @@ class Channel:
     """
 
     def __init__(self, name, product_ids):
-        """C
+        """
+        
         :param name: The name of the WebSocket channel. Possible values
           are heatbeat, ticker, level2, full, matches, or user
         :type name: str
