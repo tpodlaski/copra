bd46f07d3f93b233806dcbffbf06a9d1316358ea
Tony Podlaski <tony@podlaski.com>
2018-07-11 23:39:44 +0000

    Updated copra.websocket docstrings.

--- copra/websocket.py
+++ copra/websocket.py
@@ -31,7 +31,7 @@ class Channel:
     https://docs.gdax.com/#channels
     
     :ivar str name: The name of the WebSocket channel.
-    :ivar set of str products_ids: Product ids for the channel.
+    :ivar 'set of str' products_ids: Product ids for the channel.
     """
 
     def __init__(self, name, product_ids):
