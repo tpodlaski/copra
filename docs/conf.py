2e9fd1a23e815207a8f387a3b812eba6f25dd80e
tpodlaski <tony@podlaski.com>
2018-07-11 13:14:30 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Coinbase Pro Asyncronous Websocket Client'
+project = u'CoPrA'
 author = u"Tony Podlaski"
 this_year = time.strftime('%Y')
 if this_year != u'2018':
@@ -69,7 +69,7 @@ release = copra.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
