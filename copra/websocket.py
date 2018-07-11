3a5ed0135ad126e3bbe8ba0c86226741bfc7c7b0
Tony Podlaski <tony@podlaski.com>
2018-07-11 22:09:26 +0000

    Updated copra.websocket docstrings.

--- copra/websocket.py
+++ copra/websocket.py
@@ -40,16 +40,13 @@ class Channel:
     def __init__(self, name, product_ids):
         """
         
-        :param name: The name of the WebSocket channel. Possible values
-          are heatbeat, ticker, level2, full, matches, or user
+        :param name: The name of the WebSocket channel. Possible values are heatbeat, ticker, level2, full, matches, or user
         :type name: str
 
+        :param product_ids: A single product id (eg., 'BTC-USD') or list of product ids (eg., ['BTC-USD', 'ETH-EUR', 'LTC-BTC'])
+        :type product_ids: str or list of str
         
-        Args:
-            product_ids (str or list of str): A single product id
-                (eg., 'BTC-USD') or list of product ids (eg., ['BTC-USD',
-                'ETH-EUR', 'LTC-BTC'])
-
+        
         Raises:
             ValueError: If name not valid or product ids is empty.
         """
