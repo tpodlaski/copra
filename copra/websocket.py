63cd4f58e00386d5644641659739b447fa382cd4
Tony Podlaski <tony@podlaski.com>
2018-07-12 00:43:16 +0000

    Fixed typo in copra.websocket.Channel.__init__.

--- copra/websocket.py
+++ copra/websocket.py
@@ -47,7 +47,7 @@ class Channel:
         
         :raises ValueError: If name not valid or product ids is empty.
         """
-        self.name = name.lower
+        self.name = name.lower()
         if self.name not in ('heartbeat', 'ticker', 'level2',
                              'full', 'matches', 'user'):
             raise ValueError("invalid name {}".format(name))
