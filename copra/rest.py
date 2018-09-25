3ee48163cd9d6d83d227044b6bf9bd12c19814e0
Tony Podlaski <tony@podlaski.com>
2018-09-25 12:56:13 +0000

    Created rest.Client.get method and related test.

--- copra/rest.py
+++ copra/rest.py
@@ -6,13 +6,19 @@ for the Coinbase Pro platform.
 
 import asyncio
 import logging
+import sys
 
 import aiohttp
+
+from copra import __version__
 
 logger = logging.getLogger(__name__)
 
 URL = 'https://api.pro.coinbase.com'
 SANDBOX_URL = 'https://api-public.sandbox.pro.coinbase.com'
+
+_user_agent = 'Python/{} copra/{}'.format(
+    '.'.join([str(x) for x in sys.version_info[:3]]), __version__)
 
 class Client():
     """Asyncronous REST client for Coinbase Pro.
@@ -33,4 +39,28 @@ class Client():
         """
         await self.session.close()
         
+    async def get(self, path='/', params=None, raw=False):
+        """Base method for making GET requests.
+        
+        :param str path: The path not including the base URL of the
+            resource to be retrieved.
+            
+        :param dict params: Optional dictionary of key/value str pairs
+            to be appended to the request. The default is None.
+            
+        :param bool raw: Return the raw aiohttp.ClientResponse. This is useful
+            for debugging. The default is False.
+            
+        :returns: The response body as JSON-formatted, UTF-8 encoded dict or
+            aiohttp.ClientResponse if raw is True.
+        """
+        
+        headers = {'USER-AGENT': _user_agent}
+        
+        async with self.session.get(self.url + path, params=params, headers=headers) as resp:
+            if raw:
+                return resp
+            return await resp.json()
+            
+    
     
--- tests/test_rest.py
+++ tests/test_rest.py
@@ -9,6 +9,8 @@ Uses http://httpbin.org/ - HTTP Request & Response Service
 import asyncio
 import unittest
 
+import aiohttp
+
 from copra.rest import Client
 
 class TestClient(unittest.TestCase):
@@ -34,7 +36,6 @@ class TestClient(unittest.TestCase):
         self.loop.run_until_complete(go())
         
     def test_close(self):
-        
         async def go():
             client = Client(self.loop)
             self.assertFalse(client.session.closed)
@@ -43,3 +44,25 @@ class TestClient(unittest.TestCase):
             self.assertTrue(client.session.closed)
             
         self.loop.run_until_complete(go())
+        
+    def test_get(self):
+        async def go():
+            client = Client(self.loop, 'http://httpbin.org/')
+            params = {'key1': 'item1', 'key2': 'item2'}
+            resp = await client.get('/get', params)
+            self.assertIsInstance(resp, dict)
+            self.assertEqual(len(resp['args']), 2)
+            self.assertEqual(resp['args']['key1'], 'item1')
+            self.assertEqual(resp['args']['key2'], 'item2')
+            self.assertEqual(resp['url'], 'http://httpbin.org/get?key1=item1&key2=item2')
+            await client.close()
+            
+            client = Client(self.loop, 'http://httpbin.org/')
+            resp = await client.get('/get', raw=True)
+            self.assertIsInstance(resp, aiohttp.client_reqrep.ClientResponse)
+            await client.close()
+            
+        self.loop.run_until_complete(go())
+        
+    
+            
