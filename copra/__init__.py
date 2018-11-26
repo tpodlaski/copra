96bf574c196c1b062b9ff5bdcd759060f2606274
Tony Podlaski <tony@podlaski.com>
2018-11-26 15:12:50 +0000

    Bump version: 1.0.8 → 1.1.0

--- copra/__init__.py
+++ copra/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Tony Podlaski"""
 __email__ = 'tony@podlaski.com'
-__version__ = '1.0.8'
+__version__ = '1.1.0'
--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tpodlaski/copra',
-    version='1.0.8',
+    version='1.1.0',
     zip_safe=False,
 )
