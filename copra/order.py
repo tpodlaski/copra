05505f44a5401c95e352aff9b3d3c6b681065d53
Tony Podlaski <tony@podlaski.com>
2018-12-25 14:44:34 +0000

    Created copra.fix.Order.limit_order and market_order factory functions and related unit tests.

--- copra/order.py
+++ copra/order.py
@@ -1,10 +1,201 @@
 # -*- coding: utf-8 -*-
-"""FIX message class for the Coinbase Pro platform.
+"""Order class for the Coinbase Pro platform.
 """
 
+import asyncio
+from decimal import Decimal
 import uuid
 
+from copra.message import Message
+
 class Order:
+    """Base order class.
+    """
     
-    def __init__(self):
-        self.client_oid = str(uuid.uuid4())
+    @classmethod
+    def _create(cls, key, seq_num, side, product_id):
+        """Factory to create the base of an order and FIX message.
+        
+        Creates an order and FIX message with the attributes and fields
+        that are common to both limit and market orders.
+        
+        :param str key: The API key of the client generating the message.
+        
+        :param int seq_num: The sequence number of the message as tracked by
+            the client.
+
+        :param str side: Either buy or sell
+        
+        :param str product_id: The product id to be bought or sold.
+        
+        
+        returns: A tuple consisting of the Order object and the FIX message
+            object to place the order.
+            
+        :raises ValueError:
+        
+            * The side is not either "buy" or "sell".
+        """
+        if side not in ('buy', 'sell'):
+            raise ValueError("Invalid side: {}. Must be either buy or sell".format(side))       
+
+        order = cls()
+        msg = Message(key, seq_num, 'D', {22: '1'})
+        
+        order.client_oid = str(uuid.uuid4())
+        msg[11] = order.client_oid
+        
+        order.side = side
+        msg[54] = '1' if side == 'sell' else '2'
+        
+        order.product_id = product_id
+        msg[55] = product_id
+        
+        order.received = asyncio.Event()
+        
+        return order, msg
+
+    
+    @classmethod
+    def limit_order(cls, key, seq_num, side, product_id, size, price, 
+                                          time_in_force='GTC', stop_price=None):
+        """
+        Factory method to create a new limit order.
+        
+        Returns a new limit order and the FIX message to place it.
+
+        :param str key: The API key of the client generating the message.
+        
+        :param int seq_num: The sequence number of the message as tracked by
+            the client.
+
+        :param str side: Either buy or sell
+        
+        :param str product_id: The product id to be bought or sold.
+
+        :param float size: The quantity of the cryptocurrency to buy or sell. 
+            This parameter may also be a string.
+            
+        :param float price: The price the order is to be executed at. This 
+            paramater may also be a string to avoid floating point issues.
+            
+        :param str time_in_force: (optional) Time in force policies provide 
+            guarantees about the lifetime of an order. There are four 
+            policies: GTC (good till canceled), IOC (immediate or cancel), 
+            FOK (fill or kill), PO (post only). The default is GTC.
+        
+        :param float stop_price: (optional) The trigger price for stop orders. 
+            This may also be a string. The default is None.
+
+        :returns: A tuple consisting of the Order object and the FIX message
+            object to place the order.
+            
+        :raises ValueError:
+        
+            * The side is not either "buy" or "sell".
+            * The time_in_force is not GTC, IOC, FOK, or PO.
+            * A stop_order has post_only set to True
+        """
+
+        if time_in_force not in ('GTC', 'IOC', 'FOK', 'PO'):
+            raise ValueError('time_in_force must be GTC, IOC, FOK, or PO.')
+            
+        if stop_price and time_in_force == 'PO':
+            raise ValueError('Stop orders cannot be Post Only.')
+            
+        order, msg = cls._create(key, seq_num, side, product_id)
+
+        if stop_price:
+            order.type = 'stop limit'
+            msg[40] = '4'
+            order.stop_price = Decimal(str(stop_price))
+            msg[99] = str(order.stop_price)
+            
+        else:
+            order.type = 'limit'
+            msg[40] = '2'
+            
+        order.size = Decimal(str(size))
+        msg[38] = str(order.size)
+        
+        order.price = Decimal(str(price))
+        msg[44] = str(order.price)
+
+        order.time_in_force = time_in_force
+        msg[59] = {'GTC': '1', 'IOC': '3', 'FOK': '4', 'PO': 'P'}[order.time_in_force]
+              
+        return (order, msg)
+
+       
+    @classmethod
+    def market_order(cls, key, seq_num, side, product_id, size=None, funds=None, 
+                                                               stop_price=None):
+        """Factory method to create a new market order.
+        
+        Returns a new market order and the FIX message to place it.
+
+        :param str key: The API key of the client generating the message.
+        
+        :param int seq_num: The sequence number of the message as tracked by
+            the client.
+
+        :param str side: Either buy or sell
+        
+        :param str product_id: The product id to be bought or sold.
+
+        :param float size: The quantity of the cryptocurrency to buy or sell. 
+            Either size or funds must be set for a market order but not both.  
+            This may also be a string. The default is None. 
+
+        :param float funds: This is the amount of quote currency to be used for 
+            a purchase (buy) or the amount to be obtained from a sale (sell). 
+            Either size or funds must be set for a market order but not both. 
+            This may also be a string. The default is None.
+
+        :param float stop_price: (optional) The trigger price for stop orders. 
+            Required if stop is set. This may also be a string. The default is 
+            None.
+            
+        :returns: A tuple consisting of the Order object and the FIX message
+            object to place the order.
+            
+        :raises ValueError:
+        
+            * The side is not either "buy" or "sell".
+            * Neither size nor funds is set.
+            * Both size and funds are set
+        """
+            
+        if not (size or funds):
+                raise ValueError('Market orders must have size or funds set.')
+                
+        if size and funds:
+                raise ValueError("Market orders can't have both size and funds set.")
+                
+        order, msg = cls._create(key, seq_num, side, product_id)
+
+        if stop_price:
+            order.type = 'stop market'
+            msg[40] = '3'
+            order.stop_price = Decimal(str(stop_price))
+            msg[99] = str(order.stop_price)
+            
+        else:
+            order.type = 'market'
+            msg[40] = '1'
+        
+        if size:
+            order.size = Decimal(str(size))
+            msg[38] = str(order.size)
+            
+        if funds:
+            order.funds = Decimal(str(funds))
+            msg[152] = str(order.funds)
+            
+        return (order, msg)
+    
+    def fix_update(self,  msg):
+        pass
+        
+    #     if msg[150] == '0':         #ExcecType new
+    #         self.received.set()
--- tests/unit/fix/test_order.py
+++ tests/unit/fix/test_order.py
@@ -3,12 +3,234 @@
 """Unit tests for `copra.fix.Order` class.
 """
 
+from decimal import Decimal
+
 from asynctest import TestCase
 
-from copra.order import Order
+from copra.message import Message
+from copra.order2 import Order
+
+
+# These are made up
+TEST_KEY = 'a035b37f42394a6d343231f7f772b99d'
+TEST_SECRET = 'aVGe54dHHYUSudB3sJdcQx4BfQ6K5oVdcYv4eRtDN6fBHEQf5Go6BACew4G0iFjfLKJHmWY5ZEwlqxdslop4CC=='
+TEST_PASSPHRASE = 'a2f9ee4dx2b'
+
 
 class TestOrder(TestCase):
     
-    def test___init__(self):
-        order = Order()
+    def test__create(self):
+
+        # Invalid side
+        with self.assertRaises(ValueError):
+            Order._create(TEST_KEY, 1, 'dark', 'BTC-USD')        
+        
+        (order, msg) = Order._create(TEST_KEY, 1, 'buy', 'BTC-USD')
+        
+        self.assertIsInstance(order, Order)
         self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.side, 'buy')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertFalse(order.received.is_set())
+    
+        expected_msg = Message(TEST_KEY, 1, 'D', {22: '1', 54: '2', 55: 'BTC-USD'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+
+
+    def test_limit_order(self):
+        
+        # Invalid side
+        with self.assertRaises(ValueError):
+            Order.market_order(TEST_KEY, 1, 'dark', 'BTC-USD', .001)
+
+        # Invalid time_in_force
+        with self.assertRaises(ValueError):
+            Order.limit_order(TEST_KEY, 2, 'buy', 'BTC-USD', 100, 5, time_in_force='OPP')
+            
+        # Buy, default time_in_force
+        (order, msg) = Order.limit_order(TEST_KEY, 3, 'buy', 'BTC-USD', 3.1, 1.14)
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'limit')
+        self.assertEqual(order.side, 'buy')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.size, Decimal('3.1'))
+        self.assertEqual(order.price, Decimal('1.14'))
+        self.assertEqual(order.time_in_force, 'GTC')
+        
+        expected_msg = Message(TEST_KEY, 3, 'D', {22: '1', 40: '2', 38: '3.1', 44: '1.14', 54: '2', 55: 'BTC-USD', 59: '1'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        # Sell, PO time_in_force
+        (order, msg) = Order.limit_order(TEST_KEY, 4, 'sell', 'LTC-USD', 5, 100, time_in_force='PO')
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'limit')
+        self.assertEqual(order.side, 'sell')
+        self.assertEqual(order.product_id, 'LTC-USD')
+        self.assertEqual(order.size, Decimal('5'))
+        self.assertEqual(order.price, Decimal('100'))
+        self.assertEqual(order.time_in_force, 'PO')        
+
+        expected_msg = Message(TEST_KEY, 4, 'D', {22: '1', 40: '2', 38: '5', 44: '100', 54: '1', 55: 'LTC-USD', 59: 'P'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        # Stop order with PO time_in_force
+        with self.assertRaises(ValueError):
+            (order, msg) = Order.limit_order(TEST_KEY, 5, 'sell', 'ETH-USD', 8.5,
+                                         600, time_in_force='PO', stop_price=595)
+
+
+        # Stop loss
+        (order, msg) = Order.limit_order(TEST_KEY, 6, 'sell', 'BTC-USD', .001,
+                                                              3.5, stop_price=4)
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'stop limit')
+        self.assertEqual(order.side, 'sell')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.size, Decimal('.001'))
+        self.assertEqual(order.price, Decimal('3.5'))
+        self.assertEqual(order.time_in_force, 'GTC')
+        self.assertEqual(order.stop_price, Decimal('4'))
+
+        expected_msg = Message(TEST_KEY, 6, 'D', {22: '1', 40: '4', 38: '0.001', 
+                           44: '3.5', 54: '1', 55: 'BTC-USD', 59: '1', 99: '4'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        # Stop entry
+        (order, msg) = Order.limit_order(TEST_KEY, 7, 'buy', 'BTC-USD', .005, 
+                                                         10000, stop_price=9900)
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'stop limit')
+        self.assertEqual(order.side, 'buy')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.size, Decimal('.005'))
+        self.assertEqual(order.price, Decimal('10000'))
+        self.assertEqual(order.time_in_force, 'GTC')
+        self.assertEqual(order.stop_price, Decimal('9900'))
+
+        expected_msg = Message(TEST_KEY, 7, 'D', {22: '1', 40: '4', 38: '0.005', 
+                      44: '10000', 54: '2', 55: 'BTC-USD', 59: '1', 99: '9900'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+    def test_market_order(self):
+        
+        # Invalid side
+        with self.assertRaises(ValueError):
+            Order.market_order(TEST_KEY, 1, 'dark', 'BTC-USD', .001)
+        
+        # No funds or size
+        with self.assertRaises(ValueError):
+            Order.market_order(TEST_KEY, 2, 'buy', 'BTC-USD')
+            
+        # funds and size
+        with self.assertRaises(ValueError):
+            Order.market_order(TEST_KEY, 3, 'buy', 'BTC-USD', size=.001, funds=10000)
+            
+        # buy size
+        (order, msg) = Order.market_order(TEST_KEY, 5, 'buy', 'BTC-USD', .001)
+        
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'market')
+        self.assertEqual(order.side, 'buy')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.size, Decimal('.001'))
+        self.assertFalse(order.received.is_set())
+        
+        expected_msg = Message(TEST_KEY, 5, 'D', {22: '1', 40: '1', 38: .001, 54: '2', 55: 'BTC-USD'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        # buy funds
+        (order, msg) = Order.market_order(TEST_KEY, 6, 'buy', 'LTC-USD', funds=500)
+        
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'market')
+        self.assertEqual(order.side, 'buy')
+        self.assertEqual(order.product_id, 'LTC-USD')
+        self.assertEqual(order.funds, Decimal('500'))
+        self.assertFalse(order.received.is_set())
+        
+        expected_msg = Message(TEST_KEY, 6, 'D', {22: '1', 40: '1', 152: 500, 54: '2', 55: 'LTC-USD'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        # sell size
+        (order, msg) = Order.market_order(TEST_KEY, 7, 'sell', 'ETH-USD', .003)
+        
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'market')
+        self.assertEqual(order.side, 'sell')
+        self.assertEqual(order.product_id, 'ETH-USD')
+        self.assertEqual(order.size, Decimal('.003'))
+        self.assertFalse(order.received.is_set())
+        
+        expected_msg = Message(TEST_KEY, 7, 'D', {22: '1', 40: '1', 38: .003, 54: '1', 55: 'ETH-USD'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+
+        # sell funds
+        (order, msg) = Order.market_order(TEST_KEY, 8, 'sell', 'BTC-USD', funds=1000)
+        
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'market')
+        self.assertEqual(order.side, 'sell')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.funds, Decimal('1000'))
+        self.assertFalse(order.received.is_set())
+        
+        expected_msg = Message(TEST_KEY, 8, 'D', {22: '1', 40: '1', 152: 1000, 54: '1', 55: 'BTC-USD'})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        # stop loss
+        (order, msg) = Order.market_order(TEST_KEY, 9, 'sell', 'BTC-USD', .002, stop_price=2.2)
+        
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'stop market')
+        self.assertEqual(order.side, 'sell')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.size, Decimal('.002'))
+        self.assertEqual(order.stop_price, Decimal('2.2'))
+        self.assertFalse(order.received.is_set())
+        
+        expected_msg = Message(TEST_KEY, 9, 'D', {22: '1', 40: '3', 38: .002, 54: '1', 55: 'BTC-USD', 99: 2.2})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+
+        # stop entry
+        (order, msg) = Order.market_order(TEST_KEY, 10, 'buy', 'BTC-USD', .004, stop_price=9000)
+        
+        self.assertIsInstance(order, Order)
+        self.assertIsInstance(order.client_oid, str)
+        self.assertEqual(order.type, 'stop market')
+        self.assertEqual(order.side, 'buy')
+        self.assertEqual(order.product_id, 'BTC-USD')
+        self.assertEqual(order.size, Decimal('.004'))
+        self.assertEqual(order.stop_price, Decimal('9000'))
+        self.assertFalse(order.received.is_set())
+        
+        expected_msg = Message(TEST_KEY, 10, 'D', {22: '1', 40: '3', 38: .004, 54: '2', 55: 'BTC-USD', 99: 9000})
+        expected_msg[11] = msg[11]
+        self.assertEqual(msg, expected_msg)
+        
+        
+    # def test_received_new(self):
+    #     order = Order()
+    #     self.assertFalse(order.received.is_set())
+    #     msg = Message(TEST_KEY, 69, 8, {150: 0})
+    #     order.fix_update(msg)
+    #     self.assertTrue(order.received.is_set())
+      
