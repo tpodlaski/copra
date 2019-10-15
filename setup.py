2f1262aba63ab70e80b171624da04d7f19829ddf
Tony Podlaski <tony@podlaski.com>
2019-10-15 13:08:14 -0400

    Bump version: 1.2.8 → 1.2.9

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tpodlaski/copra',
-    version='1.2.8',
+    version='1.2.9',
     zip_safe=False,
 )
